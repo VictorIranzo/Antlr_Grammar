43b909d2904d97e62a06e2964594598d00f85510
VictorIranzo <viranzo96@gmail.com>
2018-11-11 19:25:52 +0100

    Extraída clase estática para suggester.

--- IntellisenseTest/Program.cs
+++ IntellisenseTest/Program.cs
@@ -16,35 +16,13 @@ namespace IntellisenseTest
             JavaCompiler.CompileJavaCode(args[0].Substring(0, args[0].LastIndexOf('\\') + 1));
 
             // 3. Invoke suggester.
-            Suggest(args[0].Substring(0, args[0].LastIndexOf('\\') + 1), args[0].Substring(args[0].LastIndexOf('\\') + 1).Replace(".g4", string.Empty), args[1]);
+            Suggester.Suggest(args[0].Substring(0, args[0].LastIndexOf('\\') + 1), args[0].Substring(args[0].LastIndexOf('\\') + 1).Replace(".g4", string.Empty), args[1]);
 
             // 4. Delete java and class files.
             JavaStepsDeleter.DeleteFiles(args[0].Substring(0, args[0].LastIndexOf('\\') + 1));
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
-        }
-
-        private static void Suggest(string grammarDirectory, string grammarName, string sentence)
-        {
-            Process process = new Process();
-
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardError = true;
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-            process.StartInfo.CreateNoWindow = false;
-            process.StartInfo.FileName = "java";
-
-            string urlToFolder = "file:" + grammarDirectory;
-
-            process.StartInfo.Arguments = "-jar ..\\..\\..\\antlr4-autosuggest.jar " + urlToFolder + " " + grammarName + " \"" + sentence + "\"";
-            process.Start();
-
-            Console.WriteLine(process.StandardOutput.ReadToEnd());
-
-            process.WaitForExit();
         }
     }
 }
---
+++ IntellisenseTest/Suggester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using BuildGrammar;
+using JavaUtilities;
+
+using System;
+
+using System.Diagnostics;
+
+namespace IntellisenseTest
+{
+    public static class Suggester
+    {
+        public static void Suggest(string grammarDirectory, string grammarName, string sentence)
+        {
+            Process process = new Process();
+
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.FileName = "java";
+
+            string urlToFolder = "file:" + grammarDirectory;
+
+            process.StartInfo.Arguments = "-jar ..\\..\\..\\antlr4-autosuggest.jar " + urlToFolder + " " + grammarName + " \"" + sentence + "\"";
+            process.Start();
+
+            Console.WriteLine(process.StandardOutput.ReadToEnd());
+
+            process.WaitForExit();
+        }
+    }
+}
