2026cafc7894a444b2ca0694e5745dd6a458b69f
Víctor Iranzo <viranzo96@gmail.com>
2019-06-17 00:15:54 +0200

    Añadidas interfaces para el registro de lexer y parser.

---
+++ Core/IEntityLexer.cs
@@ -0,0 +1,12 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public interface IEntityLexer<TLexer>
+        where TLexer : Lexer
+    {
+    }
+}
---
+++ Core/IEntityParser.cs
@@ -0,0 +1,12 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public interface IEntityParser<TParser>
+        where TParser : Parser
+    {
+    }
+}
